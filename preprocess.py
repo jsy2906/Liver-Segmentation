5ee2e812ed7598e95c2cfc2c1c7aa89b5ad17fa6
Seyeong <69945030+jsy2906@users.noreply.github.com>
2022-03-01 00:57:13 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -20,7 +20,8 @@ from sklearn.model_selection import train_test_split
 
 def preprocess(# niftifile_dir,
                 image_path, label_path,
-                pixdim=[1.5, 1.5, 1.0], a_min=-200, a_max=200, spatial_size=[128,128,64],
+                pixdim=[1.5, 1.5, 1.0], a_min=-200, a_max=200,
+                train_spatial_size=[128,128,64], valid_spatial_size=[128, 128, 64],
                 train_batch=2, valid_batch=2):
 
     # image_path = niftifile_dir + '/images/not_empty/*'
@@ -45,7 +46,7 @@ def preprocess(# niftifile_dir,
             CropForegroundd(keys=['image', 'label'], source_key='image'),
             RandFlipd(keys=['image', 'label'], spatial_axis=[0], prob=0.3),
             RandRotate90d(keys=['image', 'label'], prob=0.1, max_k=3),
-            Resized(keys=["image", "label"], spatial_size=spatial_size),   
+            Resized(keys=["image", "label"], spatial_size=train_spatial_size),   
             ToTensord(keys=["image", "label"]),
         ]
     )
@@ -58,7 +59,7 @@ def preprocess(# niftifile_dir,
             # Orientationd(keys=["image", "label"], axcodes="RAS"),
             ScaleIntensityRanged(keys=["image"], a_min=a_min, a_max=a_max, b_min=0.0, b_max=1.0, clip=True), 
             CropForegroundd(keys=['image', 'label'], source_key='image'),
-            Resized(keys=["image", "label"], spatial_size=spatial_size),   
+            Resized(keys=["image", "label"], spatial_size=valid_spatial_size),   
             ToTensord(keys=["image", "label"]),
         ]
     )
